c233bb3e6ec7988071d8454e7305452f818f5212
web-joyanta <web.joyanta@gmail.com>
2025-08-12 18:38:42 +0600

    jobs single data get for mongoDB unick _id pass for data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.BD_User}:${process.env.BD_Password}@cluster0.z1ypfcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,16 +28,23 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-   // jobs related apis
+    // jobs related apis
     const jobsDB = client.db("jobPortalDB");
     const jobsCollection = jobsDB.collection("jobs");
-    
-    app.get("jobs", async(req, res)=> {
+
+    app.get("/jobs", async (req, res) => {
       const jobs = jobsCollection.find();
       const result = await jobs.toArray();
       res.send(result);
     });
-    
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -46,10 +53,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/", (req, res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(port, () =>{
-    console.log(`Server is Connate Port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server is Connate Port: ${port}`);
 });
